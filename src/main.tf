aa59c91848097377ee540a165135b83d8319f832
dwill <xpositivityx@gmail.com>
2023-04-20 23:31:06 -0700

    making dns optional

--- src/main.tf
+++ src/main.tf
@@ -1,17 +1,18 @@
 locals {
-  fq_domain             = "${var.dns.sub_domain}.${data.aws_route53_zone.lookup.name}"
+  fq_domain             = "${var.dns.sub_domain}.${data.aws_route53_zone.lookup[0].name}"
   is_regional           = var.rest_api.endpoint_configuration == "REGIONAL"
   is_edge               = var.rest_api.endpoint_configuration == "EDGE"
   regional_or_edge_cert = local.is_regional ? module.acm_regional_certificate[0].certificate_arn : module.acm_edge_certificate[0].certificate_arn
-  hosted_zone_id        = split("/", var.dns.hosted_zone)[1]
+  hosted_zone_id        = var.dns.enabled ? split("/", var.dns.hosted_zone)[1] : ""
 }
 
 data "aws_route53_zone" "lookup" {
+  count   = var.dns.enabled ? 1 : 0
   zone_id = local.hosted_zone_id
 }
 
 module "acm_edge_certificate" {
-  count          = local.is_edge ? 1 : 0
+  count          = local.is_edge && var.dns.enabled ? 1 : 0
   source         = "github.com/massdriver-cloud/terraform-modules//aws/acm-certificate?ref=21b84cd"
   domain_name    = local.fq_domain
   hosted_zone_id = local.hosted_zone_id
@@ -21,20 +22,21 @@ module "acm_edge_certificate" {
 }
 
 module "acm_regional_certificate" {
-  count          = local.is_regional ? 1 : 0
+  count          = local.is_regional && var.dns.enabled ? 1 : 0
   source         = "github.com/massdriver-cloud/terraform-modules//aws/acm-certificate?ref=21b84cd"
   domain_name    = local.fq_domain
   hosted_zone_id = local.hosted_zone_id
 }
 
 module "api_gateway" {
-  source                 = "github.com/massdriver-cloud/terraform-modules//aws/api-gateway-rest-api?ref=2e621cf"
+  source                 = "github.com/massdriver-cloud/terraform-modules//aws/api-gateway-rest-api?ref=f15c309"
   name                   = var.md_metadata.name_prefix
   endpoint_configuration = var.rest_api.endpoint_configuration
   domain                 = local.fq_domain
   hosted_zone_id         = local.hosted_zone_id
   certificate_arn        = local.regional_or_edge_cert
   stage_name             = var.rest_api.stage_name
+  dns_enabled            = var.dns.enabled
 
   depends_on = [module.acm_edge_certificate, module.acm_regional_certificate]
 }
